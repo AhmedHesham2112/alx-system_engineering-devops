655233fdb06a9b7f4c4226948a8e833dee5d3577
root <root@DESKTOP-1N3TEFC>
2024-05-20 21:32:58 +0300

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""for a given employee ID, returns information about his/her To-Do list progress."""
+"""for a given employee ID, returns information
+about his/her To-Do list progress."""
 import requests
 import sys
 
 if __name__ == "__main__":
-     if len(sys.argv) > 1:
+    if len(sys.argv) > 1:
         url = "https://jsonplaceholder.typicode.com/"
         user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-        todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+        todos = requests.get(url + "todos",
+                             params={"userId": sys.argv[1]}).json()
 
         completed = []
         for todo in todos:
