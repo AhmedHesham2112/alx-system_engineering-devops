4315b7efd403dea9cda6da755884f0c0bdeb3c7b
AhmedHesham2112 <AhmedHesham2112>
2024-06-04 21:06:48 +0300

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,11 +13,14 @@ def top_ten(subreddit):
     the titles of all hot articles for a given subreddit. If no results are found
     for the given subreddit, the function should return None.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {
         "User-Agent": "script by u/medo_hesham"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        return response.json()['data']['subscribers']
-    return 0
+        posts = response.json()['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
