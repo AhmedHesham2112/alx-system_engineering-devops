4864b9ecccef0385d974d0d84772c892cb598125
AhmedHesham2112 <AhmedHesham2112>
2024-06-04 21:09:08 +0300

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,9 +9,10 @@ import requests
 
 def top_ten(subreddit):
     """
-    recursive function that queries the Reddit API and returns a list containing
-    the titles of all hot articles for a given subreddit. If no results are found
-    for the given subreddit, the function should return None.
+    recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit.
+    If no results are found for the given subreddit,
+    the function should return None.
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {
