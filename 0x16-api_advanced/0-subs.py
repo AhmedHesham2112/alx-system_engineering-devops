4a2d3ddc4a2d64624fe6c230852274851f79a3ef
AhmedHesham2112 <AhmedHesham2112>
2024-06-04 20:42:09 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,14 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "script by u/medo_hesham"
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
